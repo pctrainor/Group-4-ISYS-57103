425b165acf9b50bb0d15600a3b3f1e744a926847
pctrainor <trainorp14@gmail.com>
2024-10-26 16:29:08 -0500

    added run.py using structure from example project

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_cors import CORS
+from flasgger import Swagger # Only required if you want to use Swagger UI
+import yaml
+from api.routes import api_bp
+from pathlib import Path
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    # If you have provided an openapi.yaml file in the docs folder, load it
+    if Path.exists(Path("drone-api.yaml")):
+        with open("drone-api.yaml", "r") as file:
+            openapi_spec = yaml.safe_load(file)
+
+        swagger = Swagger(app, template=openapi_spec)
+
+    # Register Blueprints
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=5000)
