39d6f7655d25e9af6c773ee4216bf7d8d9bdba51
pctrainor <trainorp14@gmail.com>
2024-10-26 16:30:11 -0500

    added our data model to model.py

---
+++ API/model.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+@dataclass
+class Drone:
+    BUNO_ID: str
+    Drone_Model: str
+    Manufacturer: str
+    Purchase_Date: str 
+    Serial: str
+    Status: str
+    Status_Code: str
+
+@dataclass
+class FlightPlan:
+    Flight_Plan_ID: str
+    BUNO_ID: str
+    Pilot_ID: str
+    Route_ID: str
+    IsPlanned: bool
+
+@dataclass
+class Route:
+    Route_ID: str
+    Latitude: float
+    Longitude: float
+
+@dataclass
+class Pilot:
+    Pilot_ID: str
+    Pilot_Current: bool
+    Pilot_Hours: int
